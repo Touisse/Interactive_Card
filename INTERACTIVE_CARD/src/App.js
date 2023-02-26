03a931691a16e86bed2a562b6dd7836d2796cf23
Touisse <yassin.touisd@gmail.com>
2023-02-26 17:34:58 +0100

    cards are done

--- INTERACTIVE_CARD/src/App.js
+++ INTERACTIVE_CARD/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import bgMobile from "./Assets/bg-main-mobile.png";
+import bgDesktop from "./Assets/bg-main-desktop.png";
+import logo from "./Assets/card-logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section>
+        <div className="absolute -z-10">
+          <picture>
+            <source media="(min-width : 1024px" srcSet={bgDesktop} />
+            <img src={bgMobile} alt="" />
+          </picture>
+        </div>
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+          <div className="mt-10 mx-5 lg:grid lg:grid-cols-1 lg:gap-8 max-w-7xl mx-auto">
+            <article className="front-card p-5 flex flex-col justify-between">
+              <img src={logo} alt="" className="w-20 lg:w-28" />
+              <div>
+                <h2 className="text-white lg:text-3xl text-xl lg: mb-6 tracking-widest">
+                  1234 5654 5653 5334
+                </h2>
+                <ul className="flex items-center justify-between">
+                  <li className="text-white uppercase text-base lg:text-xl tracking-widest ">
+                    Yassine Touisse
+                  </li>
+                  <li className="text-white text-xl text-base lg:text-xl tracking-widest ">00/00</li>
+                </ul>
+              </div>
+            </article>
+            <article className="back-card relative lg:ml-20">
+              <p className="absolute right-10 top-32 text-lg lg:text-xl text-white tracking-widest">
+                123
+              </p>
+            </article>
+          </div>
+          <div>form</div>
+        </div>
+      </section>
+    </>
   );
 }
 
